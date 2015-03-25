cbbd67db4423b2354e5172941bed827a9f883211
Michael Girard <michael.girard@gstv.com>
2015-03-25 09:11:27 -0400

    Initial commit with files and gitignore.

---
+++ app/src/main/java/com/gstv/androidcodingexercise/Constants.java
@@ -0,0 +1,13 @@
+package com.gstv.androidcodingexercise;
+
+/**
+ * AndroidCodingExercise
+ * Created by Michael Girard on 3/25/15.
+ * Copyright 2015 GasStationTV inc. All rights reserved.
+ */
+public class Constants {
+    public static final String BASE_TAG = "ace-";
+
+    public static final String FLICKR_API_KEY = "";
+    public static final String BASE_FLICKR_API = "https://api.flickr.com/services/rest/";
+}
---
+++ app/src/main/java/com/gstv/androidcodingexercise/model/Photos.java
@@ -0,0 +1,9 @@
+package com.gstv.androidcodingexercise.model;
+
+/**
+ * AndroidCodingExercise
+ * Created by Michael Girard on 3/25/15.
+ * Copyright 2015 GasStationTV inc. All rights reserved.
+ */
+public class Photos {
+}
